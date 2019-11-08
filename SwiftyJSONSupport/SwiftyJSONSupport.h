e63d63bf4fc1a49f1f31bb2779bc11a9a02cd830
zevwings <zev.wings@gmail.com>
2019-11-08 09:52:12 +0800

    Initial Commit

---
+++ SwiftyJSONSupport/SwiftyJSONSupport.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyJSONSupport.h
+//  SwiftyJSONSupport
+//
+//  Created by zevwings on 2019/11/8.
+//  Copyright © 2019 zevwings. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyJSONSupport.
+FOUNDATION_EXPORT double SwiftyJSONSupportVersionNumber;
+
+//! Project version string for SwiftyJSONSupport.
+FOUNDATION_EXPORT const unsigned char SwiftyJSONSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyJSONSupport/PublicHeader.h>
+
+
